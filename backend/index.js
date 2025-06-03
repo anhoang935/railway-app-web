d87755e03ed07a231002a22ebb8da7a718be6776
Hoang Thien An <anhoang17935@gmail.com>
2025-06-03 20:53:28 +0700

    s

--- backend/index.js
+++ backend/index.js
@@ -60,4 +60,3 @@ app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
-export default app;
--- backend/vercel-express.js
+++
@@ -1,5 +0,0 @@
-import app from '../index.js'; // export your app without listen()
-
-export default function handler(req, res) {
-  return app(req, res); // delegate to Express
-}
