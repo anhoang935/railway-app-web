e071b3700ead66f69d8db4b327c2012775505622
S0meDudeIdk <S0meDudeIdk@users.noreply.github.com>
2025-06-07 11:35:11 +0700

    update

--- frontend/src/data/testimonials.js
+++ frontend/src/data/testimonials.js
@@ -1,12 +1,13 @@
-import user from '../images/trump.jpg';
+import trump from '../images/trump.jpg';
+import elon from '../images/elon.jpg';
 const testimonials = [
     {
       id: 1,
-      name: "Donald Trump",
-      location: "Braunau am Inn, Austria",
+      name: "Elon Musk",
+      location: "California, USA",
       comment: "The best travel experience of my life! The personalized itinerary was perfect.",
       rating: 5,
-      image: user
+      image: elon
     },
     {
       id: 2,
@@ -14,7 +15,7 @@ const testimonials = [
       location: "New York, USA",
       comment: "Exceptional service and amazing destinations. Will definitely book again!",
       rating: 4,
-      image: user
+      image: trump
     }
   ];
 
